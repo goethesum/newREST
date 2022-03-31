da44e0580067973ec535accb1ed410e77d3d676e
herman <goethesum@gmail.com>
2022-03-31 16:17:06 +0500

    implemented db package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/goethesum/newREST/internal/comment/db"
+)
 
 // Run - is going to be responsible for
 // the installation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting the application")
+
+	db, err := db.NewDatabse()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return nil
+	}
+	fmt.Println("successfully conected and pinged databse")
+
 	return nil
 }
 
